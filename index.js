3bf0e455a7dd65479761f8bf350cb7cb5c7af466
Mikhael-Yakymets <vskitano@gmail.com>
2025-03-23 21:57:43 +0000

    Deploying to gh-pages from @ Mikhael-Yakymets/fson-114-store@9cb86107cef7f73a380a40bcd73e8793653ccfb2 🚀

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-import"./assets/styles-BK7AYJoX.js";import{a as c}from"./assets/vendor-N5iQpiFS.js";const u="https://dummyjson.com/products";c.defaults.baseURL=u;async function p(){const{data:t}=await c("/category-list");return console.log(t),t}async function d(t=1){const{data:s}=await c(`?limit=12&skip=${(t-1)*12}`);return s.products}const l=document.querySelector(".categories"),_=document.querySelector(".products");function m(t){t.unshift("All");const s=t.map(a=>`<li class="categories__item">
-   <button class="categories__btn" type="button">${a}</button>
- </li>`).join("");l.insertAdjacentHTML("beforeend",s)}function g(t){const s=t.map(({id:a,thumbnail:o,title:r,brand:e,category:n,price:i})=>`<li class="products__item" data-id="${a}">
+import"./assets/styles-BK7AYJoX.js";import{a as s}from"./assets/vendor-N5iQpiFS.js";const d="https://dummyjson.com/products";s.defaults.baseURL=d;async function p(){const{data:t}=await s("/category-list");return console.log(t),t}async function l(t=1){const{data:e}=await s(`?limit=12&skip=${(t-1)*12}`);return e.products}async function g(t,e=1){const{data:c}=await s(`/category/${t}?limit=12&skip=${(e-1)*12}`);return c.products}const m=document.querySelector(".categories"),_=document.querySelector(".products");function y(t){t.unshift("All");const e=t.map(c=>`<li class="categories__item">
+   <button class="categories__btn" type="button">${c}</button>
+ </li>`).join("");m.insertAdjacentHTML("beforeend",e)}function a(t){const e=t.map(({id:c,thumbnail:o,title:r,brand:n,category:i,price:u})=>`<li class="products__item" data-id="${c}">
     <img class="products__image" src="${o}" alt=""/>
     <p class="products__title">${r}</p>
-    <p class="products__brand"><span class="products__brand--bold">Brand: ${e}</span></p>
-    <p class="products__category">Category: ${n}</p>
-    <p class="products__price">Price: $ ${i}</p>
- </li>`).join("");_.insertAdjacentHTML("beforeend",s)}async function y(){m(await p()),g(await d())}y();
+    <p class="products__brand"><span class="products__brand--bold">Brand: ${n}</span></p>
+    <p class="products__category">Category: ${i}</p>
+    <p class="products__price">Price: $ ${u}</p>
+ </li>`).join("");_.insertAdjacentHTML("beforeend",e)}const f=document.querySelector(".products");document.querySelector(".categories");async function b(t){if(t.target.nodeName==="BUTTON"){const e=t.target.textContent,c=document.querySelector(".categories__btn--active");c&&c.classList.remove("categories__btn--active"),t.target.classList.add("categories__btn--active");const o=await g(e);f.innerHTML="",a(o)}}const L=document.querySelector(".categories");async function $(){y(await p()),a(await l())}$();L.addEventListener("click",b);
 //# sourceMappingURL=index.js.map
