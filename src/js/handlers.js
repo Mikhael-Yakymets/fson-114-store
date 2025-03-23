db78719ae2bbddaceef1c117096455147026641f
pixel <pixel.koval@gmail.com>
2025-03-23 21:11:10 +0200

    added 3task

--- src/js/handlers.js
+++ src/js/handlers.js
@@ -1,1 +1,26 @@
 // Функції, які передаються колбеками в addEventListners
+import { getProductsByCategories } from "./products-api";
+import { renderProductsList } from "./render-function";
+
+const productsList = document.querySelector('.products');
+const categoriesList = document.querySelector('.categories');
+// const allButtons = document.querySelectorAll('.categories__btn')
+
+
+export async function handleClickCategory(event) {
+    if (event.target.nodeName === "BUTTON") {
+        const contentInfo = event.target.textContent;
+        const previousActiveBtn = document.querySelector('.categories__btn--active');
+        if (previousActiveBtn) {
+            previousActiveBtn.classList.remove("categories__btn--active");
+        }
+        
+        event.target.classList.add("categories__btn--active");
+        const products = await getProductsByCategories(contentInfo);
+        productsList.innerHTML = "";
+        renderProductsList(products);
+    }
+}
+
+
+
