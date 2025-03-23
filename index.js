707a4ca2a45e787e3b453d4441f54356f1f08428
pixel22kov <oleksii.kovalchuk.m@gmail.com>
2025-03-23 18:17:56 +0000

    Deploying to gh-pages from @ Mikhael-Yakymets/fson-114-store@a041e739002d47e9c4bf7377e49d041568a28a54 🚀

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-import"./assets/styles-BK7AYJoX.js";import{a as o}from"./assets/vendor-N5iQpiFS.js";const n="https://dummyjson.com/products";o.defaults.baseURL=n;async function s(){const{data:t}=await o("/category-list");return console.log(t),t}const c=document.querySelector(".categories");function i(t){t.unshift("All");const e=t.map(a=>`<li class="categories__item">
+import"./assets/styles-BK7AYJoX.js";import{a as c}from"./assets/vendor-N5iQpiFS.js";const u="https://dummyjson.com/products";c.defaults.baseURL=u;async function p(){const{data:t}=await c("/category-list");return console.log(t),t}async function d(t=1){const{data:s}=await c(`?limit=12&skip=${(t-1)*12}`);return s.products}const l=document.querySelector(".categories"),_=document.querySelector(".products");function m(t){t.unshift("All");const s=t.map(a=>`<li class="categories__item">
    <button class="categories__btn" type="button">${a}</button>
- </li>`).join("");c.insertAdjacentHTML("beforeend",e)}async function r(){i(await s())}r();
+ </li>`).join("");l.insertAdjacentHTML("beforeend",s)}function g(t){const s=t.map(({id:a,thumbnail:o,title:r,brand:e,category:n,price:i})=>`<li class="products__item" data-id="${a}">
+    <img class="products__image" src="${o}" alt=""/>
+    <p class="products__title">${r}</p>
+    <p class="products__brand"><span class="products__brand--bold">Brand: ${e}</span></p>
+    <p class="products__category">Category: ${n}</p>
+    <p class="products__price">Price: $ ${i}</p>
+ </li>`).join("");_.insertAdjacentHTML("beforeend",s)}async function y(){m(await p()),g(await d())}y();
 //# sourceMappingURL=index.js.map
